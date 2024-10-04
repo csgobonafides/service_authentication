963ec9c1740966d711fbfefc780101b0054bdf64
Shiro Carna <alexsandr.ejik@gmail.com>
2024-10-04 17:36:58 +0500

    ksksks

--- src/middle_ware/time_meddle.py
+++ src/middle_ware/time_meddle.py
@@ -10,7 +10,7 @@ class MyMiddle(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next: Callable):
         start_time = monotonic()
         response = await call_next(request)
-        finish_time = monotonic() - start_time
+        elapsed = 1000.0 * (monotonic() - start_time)
+        finish_time = "{:0.6f}|ms".format(elapsed)
         logger.info(f'запрос эндпоинту - {str(request.url)[22:]} - обработан за {finish_time}.')
-        print(str(request.url))
         return response
